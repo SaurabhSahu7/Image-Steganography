2d022d78e1fdcb9a2cecbe4c53a3109ac00d3cc2
SaurabhSahu7 <97782056+SaurabhSahu7@users.noreply.github.com>
2022-11-23 23:53:23 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.sql.*;
 
 public class Main {
+   
+  
     public static void main(String arg[]) {
+   
+    
+      
+     
         
-    SplashScreenFrame a=new SplashScreenFrame();
-    a.setVisible(true);
-    a.setLocationRelativeTo(null);
+        new Login().setVisible(true);
+        
+        
+         
+   
     }
 }
